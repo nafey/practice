fb5642d46d15a893a279de462a1248e3704b50e8
ynafey <ynafey@gmail.com>
2016-12-16 02:04:27 +0530

    Queue

---
+++ Queue.java
@@ -0,0 +1,39 @@
+public class Queue<T> {
+	public Node<T> head;
+	public Node<T> tail;
+	
+	public boolean isEmpty() {
+		return ((head == null) && (tail == null));
+	}
+	
+	public void enqueue(T val) {
+		Node n = new Node<>(val);
+		if (this.isEmpty()) {
+			tail = head = n;
+		} else {
+			tail.next = n;
+			tail = n;
+		}
+	}
+	
+	public T dequeue() {
+		T ret = this.head.val;
+		head = head.next;
+		
+		if (head == null) tail = null;
+		
+		return ret;
+	}
+	
+	public static void main(String[] args) {
+		Queue<Integer> queue = new Queue<>();
+		queue.enqueue(5);
+		queue.enqueue(51);
+		queue.enqueue(15);
+		
+		while (!queue.isEmpty()) {
+			System.out.println(queue.dequeue());
+		}
+		System.out.println("Hello Queue!");
+	}
+}
