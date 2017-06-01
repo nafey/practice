e3f9333cd5168ff7b4c7ca901a4a2e2dc26447e8
ynafey <ynafey@gmail.com>
2017-06-02 01:49:17 +0530

    Copy array removed

--- MSDRadix.java
+++ MSDRadix.java
@@ -14,7 +14,6 @@ public class MSDRadix {
 	// Limited implementation. No duplicate vals. Only digits less than 6.
 	public void sort(String[] vals, String[] aux, int lo, int high, int radix, int pos) {
 		int[] count = new int[radix + 2];
-		int[] copy = new int[radix + 2];
 		
 		// build cumulates
 		for (int i = lo; i < high; i++) {
@@ -31,37 +30,41 @@ public class MSDRadix {
 			count[i] = count[i - 1] + count[i];
 		}
 		
-		// copy which before count is changed
-		for (int i = 0; i < copy.length; i++) {
-			copy[i] = count[i];
-		}
-		
 		// sorting to aux
 		for (int i = lo; i < high; i++) {
 			int dig = this.dig(vals[i], pos);
 			aux[count[dig + 1]] = vals[i];
 			count[dig + 1]++;
 		}
-		
+			
 		// copying back to vals
 		for (int i = lo; i < high; i++) {
 			vals[i] = aux[i];
 		}		
 		
 		// call sub sorts
-		for (int i = 1; i < copy.length; i++) {
-			lo = copy[i - 1];
-			high = copy[i];
-						
+		lo = count[0];
+		for (int i = 0; i < count.length - 1; i++) {
+			high = count[i];
+			
 			if (high - lo > 1) {
 				this.sort(vals, aux, lo, high, radix, pos + 1);
 			}
+			
+			lo = count[i];
 		}
+			
 	}
 	
 	public static void main(String[] args) {
 		String[] vals = {
 			"402",
+			"123",
+			"14200",
+			"6331",
+			"3314",
+			"5231",
+			"0123",
 			"144",
 			"31",
 			"614",
@@ -72,7 +75,8 @@ public class MSDRadix {
 			"652",
 			"254",
 			"5205",
-			"13"
+			"13",
+			"02"
 		};
 				
 		String[] aux = new String[vals.length];		
